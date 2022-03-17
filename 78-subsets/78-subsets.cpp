fdf11cb5de1a25537b04197a9fd1369aee77c496
LencyBhalodiya <79202423+LencyBhalodiya@users.noreply.github.com>
2022-03-17 19:13:20 +0530

    Time: 4 ms (53.64%), Space: 7.1 MB (84.22%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,24 +1,20 @@
 class Solution {
-private:
- vector<vector<int>> ans;
-private:
-void solve(vector<int> &nums,vector<int> temp,int i)
-{
-    if(i == nums.size())
-     ans.push_back(temp);
-    else{
-        temp.push_back(nums[i]); //add element to array
-        solve(nums,temp,i+1);
-        temp.pop_back(); //backtracking while returning
-        
-        solve(nums,temp,i+1); // don't add element to array
-    }
-   
-}
+
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
-        solve(nums,temp,0);
+        vector<vector<int>> ans;
+        int p = pow(2,nums.size());  
+        for(int i = 0 ; i < p; i++)
+        {
+             temp.clear();
+            for(int j = 0 ; j < nums.size() ; j++)
+            {
+                if((( 1<<j )& i)!=0)
+                  temp.push_back(nums[j]);
+            }
+            ans.push_back(temp);
+        }
         return ans;
     }
 };
